9c221e81d36f4b865f107765e74db16612e2cb34
altaygileren <altaygileren@gmail.com>
2018-11-14 04:41:25 -0500

    Updates

---
+++ precache-manifest.ab407c68ca84ad270950d999cdb9730b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/humanfund/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/humanfund/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/humanfund/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/humanfund/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "8ffeffa668fa07f9a6c6",
+    "url": "/humanfund/static/js/runtime~main.8ffeffa6.js"
+  },
+  {
+    "revision": "7ef9cefb2beb6e6ca3de",
+    "url": "/humanfund/static/js/main.7ef9cefb.chunk.js"
+  },
+  {
+    "revision": "9129b1e44be9853660c9",
+    "url": "/humanfund/static/js/1.9129b1e4.chunk.js"
+  },
+  {
+    "revision": "7ef9cefb2beb6e6ca3de",
+    "url": "/humanfund/static/css/main.210dc8d1.chunk.css"
+  },
+  {
+    "revision": "9129b1e44be9853660c9",
+    "url": "/humanfund/static/css/1.bbf884df.chunk.css"
+  },
+  {
+    "revision": "158cc561467f291ea9cca5559843186c",
+    "url": "/humanfund/index.html"
+  }
+];
